23bc52041ca380b209ff3bf75743062162551207
sagnikbera <sagnikberaofficial@gmail.com>
2025-09-22 21:03:52 +0530

    Update server.js

--- ownNginx/server.js
+++ ownNginx/server.js
@@ -7,7 +7,23 @@ const port = 3000
 const server = http.createServer((req , res) => {
     const filepath = path.join(__dirname, req.url === '/' ? "index.html" : "req.url" );
     const extName = String(path.extname(filepath)).toLowerCase();
-    
+
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/javascript',
+        '.png': 'text/png',
+    }
+
+    const contentType = mimeTypes[extName] || 'application/octet-stream';
+
+    fs.readFile(filepath , (err , content) => {
+        if(err) {
+
+        } else {
+            res.writeHead
+        }
+    })
 });
 
 server.listen(port , () => {
