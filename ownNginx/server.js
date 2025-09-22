2362df07ef8a7b0eb2cd370f8868dfae7bd4d8db
sagnikbera <sagnikberaofficial@gmail.com>
2025-09-23 05:27:39 +0530

    Update server.js

--- ownNginx/server.js
+++ ownNginx/server.js
@@ -1,31 +1,44 @@
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-const port = 3000
+const port = 3000;
 
-const server = http.createServer((req , res) => {
-    const filepath = path.join(__dirname, req.url === '/' ? "index.html" : "req.url" );
-    const extName = String(path.extname(filepath)).toLowerCase();
+const server = http.createServer((req, res) => {
+  let filepath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
 
-    const mimeTypes = {
-        '.html': 'text/html',
-        '.css': 'text/css',
-        '.js': 'text/javascript',
-        '.png': 'text/png',
+  //   If no extension, assume .html
+  if (!path.extname(filepath)) {
+    filepath += ".html";
+  }
+
+  console.log(filepath);
+
+  const extName = String(path.extname(filepath)).toLowerCase();
+
+  const mimeTypes = {
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".png": "text/png",
+  };
+
+  const contentType = mimeTypes[extName] || "application/octet-stream";
+
+  fs.readFile(filepath, (err, content) => {
+    if (err) {
+      if (err.code === "ENOENT") {
+        // Error NO ENTry
+        res.writeHead(404, { "content-type": "text/html" });
+        res.end("404 : File Not Found");
+      }
+    } else {
+      res.writeHead(200, { "content-Type": contentType });
+      res.end(content, "utf-8");
     }
-
-    const contentType = mimeTypes[extName] || 'application/octet-stream';
-
-    fs.readFile(filepath , (err , content) => {
-        if(err) {
-
-        } else {
-            res.writeHead
-        }
-    })
+  });
 });
 
-server.listen(port , () => {
-    console.log(`Server is listing on port ${port}`);
-})
+server.listen(port, () => {
+  console.log(`Server is listing on port ${port}`);
+});
